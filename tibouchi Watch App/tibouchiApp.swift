cd75d4b98c139d23ebad31dd7d875460e1c0f5b5
Maxime Gallo <pro.maxgallo@gmail.com>
2025-05-10 17:29:32 +0200

    Initial Commit

---
+++ tibouchi Watch App/tibouchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  tibouchiApp.swift
+//  tibouchi Watch App
+//
+//  Created by maxime gallo on 10/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct tibouchi_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
